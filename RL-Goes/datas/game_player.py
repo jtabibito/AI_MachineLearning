e8b116f5dc3b34167b701db58e572f0c77b8b1ee
JTabibito(Ji Lin) <34726874+jtabibito@users.noreply.github.com>
2022-03-25 19:08:52 +0800

    更新棋盘状态数据

---
+++ RL-Goes/datas/game_player.py
@@ -0,0 +1,27 @@
+import sys
+
+from com_moudles import Tuple
+
+class Player:
+    def __init__(self, placeholder):
+        self._prompt = 'Please input your choice: (ex. 1 2) '
+        self.placeholder = placeholder
+
+    @property
+    def id(self):
+        return self.placeholder
+
+    @property
+    def get_state(self):
+        pass
+
+    def set_state(self, *args):
+        pass
+
+    def reset(self):
+        pass
+
+    def input(self) -> Tuple:
+        sys.stdout.flush()
+        msg = input(self._prompt).split()
+        return tuple(map(int, msg))
